be5a2a154f7c97681f16d52776c605624031a971
mukeshnegi2108 <37812767+mukeshnegi2108@users.noreply.github.com>
2020-07-30 01:34:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform-auto" {
   ami           = "ami-08f3d892de259504d"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
 
   tags = {
     Name = "repository-build"
